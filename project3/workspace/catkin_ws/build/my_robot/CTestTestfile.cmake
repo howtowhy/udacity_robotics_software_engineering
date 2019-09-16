34a34b9e14240fe49cff86181109c5e2e681d8eb
Seo Yeon Stella Yang <howtowhy@gmail.com>
2019-09-16 09:15:50 +0000

    project3

---
+++ project3/workspace/catkin_ws/build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nvidia/udacity_robotics_software_engineering/project3/workspace/catkin_ws/src/my_robot
+# Build directory: /home/nvidia/udacity_robotics_software_engineering/project3/workspace/catkin_ws/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
